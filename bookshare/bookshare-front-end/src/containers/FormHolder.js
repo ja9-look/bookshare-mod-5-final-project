9e567d428306fe0e57947258278809657493c306
ja9-look <janineluk95@gmail.com>
2019-02-25 09:57:15 +0000

    added video as background and changed button onhover

--- bookshare/bookshare-front-end/src/containers/FormHolder.js
+++ bookshare/bookshare-front-end/src/containers/FormHolder.js
@@ -6,6 +6,9 @@ const FormHolder = (props) => {
 
     return (
             <div className={'form_holder'}>
+            <video autoPlay loop className={'video-background'} muted playsInline>
+                <source src={require('../images/buch8.mp4')} type="video/mp4" />
+            </video>
                 <p>bookshare</p>
                 <button className={'signUpFormToggler'} onClick={props.handleSignUpToggle}>sign up</button>
                 <button className={'loginFormToggler'} onClick={props.handleLoginToggle}>login</button>
